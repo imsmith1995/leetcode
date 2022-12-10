ebb1af5948b9c12bfeebb4a5af30a0f32561af95
Ian Smith <imsmith1995@gmail.com>
2022-12-10 18:24:00 -0500

    leetcode Running Sum of 1d Array

---
+++ Running_Sum_of_1d_Array/Running_Sum_of_1d_Array/Program.cs
@@ -0,0 +1,19 @@
+﻿//Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+//Return the running sum of nums.
+public class Solution
+{
+    public int[] RunningSum(int[] nums)
+    {
+        int[] ans = new int[nums.Length];
+        int counter = 0;
+        int adder = 0;
+        foreach (int i in nums)
+        {
+            adder += i;
+            ans[counter] = adder;
+            counter++;
+        }
+        return ans;
+    }
+}

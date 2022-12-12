4166dba6c8e9f2df1ecabf4b2ae33c798cb25276
Ian Smith <imsmith1995@gmail.com>
2022-12-12 15:06:06 -0500

    updated problem

--- Number_of_Substrings_With_Only_1s/Number_of_Substrings_With_Only_1s/Program.cs
+++ Number_of_Substrings_With_Only_1s/Number_of_Substrings_With_Only_1s/Program.cs
@@ -1,6 +1,7 @@
 ﻿/*
  Given a binary string s, return the number of substrings with all characters 1's.
  Since the answer may be too large, return it modulo 10^9 + 7.
+
 */
 public class Solution
 {

82c9001c877cc1362fcaea5c0bfa318fa62fda11
Ian Smith <imsmith1995@gmail.com>
2022-12-12 13:52:07 -0500

    leetcode practice Number of Good Pairs

---
+++ Number_of_Good_Pairs/Number_of_Good_Pairs/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j. 
+*/
+
+public class Solution
+{
+    public int NumIdenticalPairs(int[] nums)
+    {
+        int goodPairs = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j] && i < j)
+                {
+                    goodPairs++;
+                }
+            }
+        }
+        return goodPairs;
+    }
+}

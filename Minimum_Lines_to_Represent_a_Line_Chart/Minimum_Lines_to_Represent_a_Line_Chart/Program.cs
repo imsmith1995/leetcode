ace779e48a14d517b7259a84eef1cd0b7448ab0f
Ian Smith <imsmith1995@gmail.com>
2022-12-20 21:54:25 -0500

    updated solution

--- Minimum_Lines_to_Represent_a_Line_Chart/Minimum_Lines_to_Represent_a_Line_Chart/Program.cs
+++ Minimum_Lines_to_Represent_a_Line_Chart/Minimum_Lines_to_Represent_a_Line_Chart/Program.cs
@@ -1,8 +1,8 @@
 ﻿
 int[][] stockPrices = new int[][] {
-    new int[]{1, 7 },
     new int[]{2, 6 },
     new int[]{3, 5 },
+    new int[]{1, 7 },
     new int[]{4, 4 },
     new int[]{5, 4 },
     new int[]{6, 3 },
@@ -14,18 +14,30 @@ MinimumLines(stockPrices);
 
 int MinimumLines(int[][] stockPrices)
 {
-    int lines = 0;
-    decimal[] slope = new decimal[stockPrices.Length];
-    for (int i = 0; i < stockPrices.Length - 1; i++)
+    int lines = 1;
+    decimal[] slope = new decimal[stockPrices.Length-1];
+    int[][] sortedPrices = stockPrices;
+    for(int j = 0; j < sortedPrices.Length-1; j++)
     {
-        int x1 = stockPrices[i][0];
-        int y1 = stockPrices[i][1];
-        int x2 = stockPrices[i + 1][0];
-        int y2 = stockPrices[i + 1][1];
+        int[] holder = new int[2];
+        if (sortedPrices[j][0] > sortedPrices[j + 1][0])
+        {
+            holder = sortedPrices[j + 1]; 
+            sortedPrices[j + 1] = sortedPrices[j];
+            sortedPrices[j] = holder;
+            j = -1;
+        }
+    }
+    for (int i = 0; i < sortedPrices.Length - 1; i++)
+    {
+        int x1 = sortedPrices[i][0];
+        int y1 = sortedPrices[i][1];
+        int x2 = sortedPrices[i + 1][0];
+        int y2 = sortedPrices[i + 1][1];
 
         slope[i] = ((y2 - y1) / (x2 - x1));
     }
-    for (int k = 0; k < slope.Length - 1; k++)
+    for (int k = 0; k < slope.Length-1; k++)
     {
         if (slope[k] != slope[k + 1])
         {

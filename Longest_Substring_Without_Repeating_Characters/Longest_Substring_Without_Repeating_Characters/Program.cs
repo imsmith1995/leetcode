f324406481aa16884e5190a58c68ac36db7add65
Ian Smith <imsmith1995@gmail.com>
2022-12-21 13:19:39 -0500

    Longest Substring Without Repeating Characters - More Progress

--- Longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters/Program.cs
+++ Longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string s = "pwwkew";
+LengthOfLongestSubstring(s);
+int LengthOfLongestSubstring(string s)
+{
+    if (s == null || s == "") { return 0; };
+
+    int ans = 0;
+    List<int> substringLengths = new();
+    char[] str = s.ToArray();
+    int counter = 0;
+    int subLength = 1;
+
+    foreach(char c in str)
+    {
+        counter++;
+        List<char> compare = new();
+        compare.Add(c);
+        for (int i = counter; i < str.Length; i++)
+        {
+            if (compare.Contains(str[i]))
+            {
+                substringLengths.Add(subLength);
+                subLength = 1; break;
+            }
+            else
+            {
+                subLength++;
+                compare.Add(str[i]);
+            }
+        }
+    }
+    ans = substringLengths.Max();
+
+    return ans;
+}

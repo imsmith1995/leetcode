e4bacc230a587ea8828526e8f125d1b98078140a
Ian Smith <imsmith1995@gmail.com>
2022-12-20 21:18:48 -0500

    leetcode Minimum Lines to Represent a Line Chart

---
+++ Minimum_Lines_to_Represent_a_Line_Chart/Minimum_Lines_to_Represent_a_Line_Chart/Program.cs
@@ -0,0 +1,37 @@
+﻿
+int[][] stockPrices = new int[][] {
+    new int[]{1, 7 },
+    new int[]{2, 6 },
+    new int[]{3, 5 },
+    new int[]{4, 4 },
+    new int[]{5, 4 },
+    new int[]{6, 3 },
+    new int[]{7, 2 },
+    new int[]{8, 1 }
+    };
+
+MinimumLines(stockPrices);
+
+int MinimumLines(int[][] stockPrices)
+{
+    int lines = 0;
+    decimal[] slope = new decimal[stockPrices.Length];
+    for (int i = 0; i < stockPrices.Length - 1; i++)
+    {
+        int x1 = stockPrices[i][0];
+        int y1 = stockPrices[i][1];
+        int x2 = stockPrices[i + 1][0];
+        int y2 = stockPrices[i + 1][1];
+
+        slope[i] = ((y2 - y1) / (x2 - x1));
+    }
+    for (int k = 0; k < slope.Length - 1; k++)
+    {
+        if (slope[k] != slope[k + 1])
+        {
+            lines++;
+        }
+    }
+    return lines;
+
+}

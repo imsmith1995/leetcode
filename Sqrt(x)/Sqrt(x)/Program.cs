902ed33ae64d3d07718b23d9ab37fa2119396c72
Ian Smith <imsmith1995@gmail.com>
2023-02-08 14:22:29 -0500

    update

--- Sqrt(x)/Sqrt(x)/Program.cs
+++ Sqrt(x)/Sqrt(x)/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int x = 4;
+MySqrt(x);
+
+int MySqrt(int x)
+{
+    if (x == 1)
+    {
+        return x;
+    }
+    for (double i = 0; i <= x; i++)
+    {
+
+        if (i * i > x)
+        {
+            return Convert.ToInt32(i - 1);
+            break;
+        }
+    }
+    return 0;
+}

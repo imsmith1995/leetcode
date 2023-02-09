0895abe87a8bbf8efc70909bb781b81cc158170d
Ian Smith <imsmith1995@gmail.com>
2023-02-09 17:40:43 -0500

    Length of Last Work

---
+++ Valid_Phone_Numbers/Valid_Phone_Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal 
+substring
+consisting of non-space characters only.
+ */
+
+var s = "    orld";
+LengthOfLastWord(s);
+int LengthOfLastWord(string s)
+{
+    var holder = s.ToArray();
+    int spaceCounter = 0;
+    int spaceChecker = 0;
+    int ans = 0;
+    foreach(var item in holder)
+    {
+        if(item is ' ')
+        {
+            spaceCounter++;
+        }
+    }
+    for(int i = 0; i < holder.Length; i++)
+    {
+        if( spaceCounter == spaceChecker)
+        {
+            ans++;
+        }
+        if (holder[i] is ' ')
+        {
+            spaceChecker++;
+        }
+    }
+
+    return ans;
+}

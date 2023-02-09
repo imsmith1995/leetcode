dcf2d0ccb20fbc5792d1d11be80e048a5977e64f
Ian Smith <imsmith1995@gmail.com>
2023-02-09 17:22:18 -0500

    Pascal's Triangle Solution

--- Pascals_Triangle/Pascals_Triangle/Program.cs
+++ Pascals_Triangle/Pascals_Triangle/Program.cs
@@ -1,19 +1,25 @@
-﻿
-int x = 5;
-Generate(x);
-IList<IList<int>> Generate(int numRows)
-{
-    var triangle = new List<IList<int>> { new List<int> { 1 }};
+﻿/*
+Given an integer numRows, return the first numRows of Pascal's triangle.
 
-    for(int i = 1; i < numRows; i++)
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+ */
+
+public class Solution
+{
+    public IList<IList<int>> Generate(int numRows)
     {
-        var level = new List<int> { 1 };
-        for (int j = 1; j < i ; j++)
+        var triangle = new List<IList<int>> { new List<int> { 1 } };
+
+        for (int i = 1; i < numRows; i++)
         {
-            level.Add(triangle[triangle.Count - 1][j - 1] + triangle[triangle.Count - 1][j]);
+            var level = new List<int> { 1 };
+            for (int j = 1; j < i; j++)
+            {
+                level.Add(triangle[triangle.Count - 1][j - 1] + triangle[triangle.Count - 1][j]);
+            }
+            level.Add(1);
+            triangle.Add(level);
         }
-        level.Add(1);
-        triangle.Add(level);
+        return triangle;
     }
-    return triangle;
 }

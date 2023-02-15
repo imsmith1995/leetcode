fac1959a24c90b2329f8eaa80cf13bec1a4232ce
Ian Smith <imsmith1995@gmail.com>
2023-02-15 15:43:26 -0500

    Plus One Continued

--- Plus_One/Plus_One/Program.cs
+++ Plus_One/Plus_One/Program.cs
@@ -7,6 +7,7 @@ int[] PlusOne(int[] digits)
 {
     var total = 0;
     var multiple = 1;
+    int[] ans = new int[3];
     if(digits.Length > 1)
     {
         for(int i = 1; i< digits.Length; i++)
@@ -14,15 +15,19 @@ int[] PlusOne(int[] digits)
             multiple = multiple * 10;
         }
     }
+    var deltaMultiple = multiple;
     foreach(int digit in digits)
     {
-        var deltaMultiple = multiple;
         total += digit * deltaMultiple;
-        multiple = multiple / 10;
+        deltaMultiple = deltaMultiple / 10;
     }
     total++;
+    var betaMultiple = multiple;
+    //issues here. needs addressing
     for(int i = 0; i< digits.Length; i++)
     {
+        var primeAns = total;
+        ans[i] = primeAns/betaMultiple;
 
     }
     return new int[2];
